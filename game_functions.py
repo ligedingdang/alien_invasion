834d8c975354dc1adba5636516f24a91cdff0d0e
Leo_X_Y <1367168783@qq.com>
2020-10-29 12:07:17 +0800

    finalship

--- game_functions.py
+++ game_functions.py
@@ -9,10 +9,7 @@ def check_keydown_events(event , ai_settings , screen , ship , bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        #创建新子弹，并将其加入到编组bullets中
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings , screen , ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings , screen , ship , bullets)
 
 def check_keyup_events(event , ship):
     """响应松开"""
@@ -44,3 +41,19 @@ def update_screen(ai_settings , screen , ship , bullets):
     #让最近绘制的屏幕可见
     pygame.display.flip()
             
+def update_bullets(bullets):
+    """更新子弹的位置并删除已消失的子弹"""
+    #更新子弹的位置
+    bullets.update()
+
+    #删除已消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings , screen , ship , bullets):
+    """如果还没有到达限制则发射一颗子弹"""
+    #创建新子弹，并将其加入到编组bullets中
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings , screen , ship)
+        bullets.add(new_bullet)
