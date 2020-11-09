4e19db35c9cfadde6203e60195b19ac268e1b374
Leo_X_Y <1367168783@qq.com>
2020-11-09 16:51:50 +0800

    delete dead aliens

--- game_functions.py
+++ game_functions.py
@@ -45,15 +45,15 @@ def update_screen(ai_settings , screen , ship , aliens , bullets):
     #让最近绘制的屏幕可见
     pygame.display.flip()
             
-def update_bullets(bullets):
+def update_bullets(aliens , bullets):
     """更新子弹的位置并删除已消失的子弹"""
     #更新子弹的位置
     bullets.update()
 
-    #删除已消失的子弹
-    for bullet in bullets.copy():
-        if bullet.rect.bottom <= 0:
-            bullets.remove(bullet)
+    # 检查是否有子弹击中了外星人
+    # 如果是这样，就删除相应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets , aliens , True , True)
+
 
 def fire_bullet(ai_settings , screen , ship , bullets):
     """如果还没有到达限制则发射一颗子弹"""
