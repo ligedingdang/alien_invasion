072c9983c3998f82ced0786cbc6e914b25846288
Leo_X_Y <1367168783@qq.com>
2020-11-09 17:47:30 +0800

    mouse

--- game_functions.py
+++ game_functions.py
@@ -22,7 +22,7 @@ def check_keyup_events(event , ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings , screen , stats , play_button , ship , bullets):
+def check_events(ai_settings , screen , stats , play_button , ship , aliens , bullets):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -33,12 +33,27 @@ def check_events(ai_settings , screen , stats , play_button , ship , bullets):
            check_keyup_events(event , ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x , mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats , play_button , mouse_x , mouse_y)
+            check_play_button(ai_settings , screen , stats , play_button , ship , aliens , bullets , mouse_x , mouse_y)
     
-def check_play_button(stats , play_button , mouse_x , mouse_y):
+def check_play_button(ai_settings , screen , stats , play_button , ship , aliens , bullets , mouse_x , mouse_y):
     """在玩家单击Play按钮时开始新游戏"""
-    if play_button.rect.collidepoint(mouse_x , mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x , mouse_y)
+    if button_clicked and not stats.game_active:
+
+        #隐藏光标
+        pygame.mouse.set_visible(False)
+
+        #重置游戏统计信息
+        stats.reset_stats()
         stats.game_active = True
+        
+        #清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty()
+
+        #创建一群新的外星人并让飞船居中
+        create_fleet(ai_settings , screen , ship , aliens)
+        ship.center_ship()
            
 def update_screen(ai_settings , screen , stats , ship , aliens , bullets , play_button):
     """更新屏幕上的图像，并切换到新屏幕"""
@@ -150,6 +165,7 @@ def ship_hit(ai_settings , stats , screen , ship , aliens , bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_aliens_bottom(ai_settings , stats , screen , ship , aliens , bullets):
